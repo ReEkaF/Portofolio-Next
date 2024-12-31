4d7284146f471728adeca3c21293058233adbd0d
ReEkaF <andreeka852@gmail.com>
2024-12-31 19:32:37 +0700

    etstttihdfes

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -8,7 +8,6 @@ import { cn } from "@/lib/utils";
 
 
 import { BackgroundGradientAnimation } from "./GradientBg";
-import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
 
